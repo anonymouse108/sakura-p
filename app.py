59e3fd27f89335888ee6aa934b541db87bebe270
anonymouse108 <lakshitffmax@gmail.com>
2026-08-12 23:54:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROXY_FILE = os.path.join(BASE_DIR, "proxies.txt")
 
-REFRESH_INTERVAL = 120
+REFRESH_INTERVAL = 240
 
 last_update = "Never"
 proxy_count = 0
